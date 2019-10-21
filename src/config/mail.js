fc8808de1f2bd24c0af51154e379cf96dc37503e
Hikaro Cordeiro <hikaro.cordeiro@gmail.com>
2019-10-21 16:14:22 -0300

    nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '20a45e98a2e151',
+    pass: '3a5e013a824e3e',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
